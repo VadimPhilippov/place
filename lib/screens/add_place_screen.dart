22f37a00fbc05a74566896edcf36c37fddc88c61
VadimPhilippov <vadfil@gmail.com>
2019-10-14 16:34:19 +0200

    Fetch the User Coordinates .

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 
 import '../widgets/image_input.dart';
 import '../providers/great_places.dart';
+import '../widgets/location_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -56,6 +57,10 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                       height: 10,
                     ),
                     ImageInput(_selectImage),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    LocationInput(),
                   ],
                 ),
               ),
